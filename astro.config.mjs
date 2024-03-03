bd6c0a81635255f8ea46750430d8970cdb504353
Andre Vollrath <andre.vollrath@talentadore.com>
2024-03-03 13:00:04 +0200

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import solid from '@astrojs/solid-js';
 
 export default defineConfig({
 
-    integrations: [solid()],
+    integrations: [],
     site: 'https://avollrath.github.io',
+    output: 'static',
+    outDir: './docs',
+    build: {
+        assets: 'astro'
+      }
 });
 
 
