3f27abf68986d6da872fef524619d37bf1904042
Andre Vollrath <andre.vollrath@talentadore.com>
2024-12-28 23:41:57 +0200

    color update

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,22 +23,33 @@ export default {
 
       gradientColorStops: theme => ({
         ...theme('colors'),
-        'gradient': 'linear-gradient(45deg, #F6723A, #F6723A)',
+        'gradient': 'linear-gradient(45deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
       }),
+
+      backgroundImage: {
+        'about-gradient': 'linear-gradient(32deg, rgba(9,9,121,1) 0%, rgba(0,212,255,1) 100%)',
+      },
+      textColor: {
+        'transparent': 'transparent',
+      },
 
       colors: {
         'dark-bg': '#0d0d0d',  // Adjust the hex value to match your design
         'dark-card': '#1a1a1a', // Adjust the hex value to match your design
         'darker-card': '#121212',
         'dark-accent': '#757575', // Adjust the hex value to match your design
-        'dark-text': '#F6723A', // Adjust the hex value to match your design
-        'white': '#fdf1e7',
-        'orange': '#F6723A',
+        'dark-text': '#038cd0', // Adjust the hex value to match your design
+        'white': '#FFF',
+        'orange': '#038cd0',
       },
 
   fontFamily: {
-        'header': ['Young Serif', 'serif'], // For headers, using Montserrat with weight 700
-        'body': ['Syne Mono', 'sans-serif'], // For body text, using Montserrat with weight 400
+        'header': ['Poppins', 'sans-serif'], // For headers, using Montserrat with weight 700
+        'body': ['Poppins', 'sans-serif'], // For body text, using Montserrat with weight 400
+      },
+      fontWeight: {
+        'header': '700', // Adding a custom weight
+        'body': '400',
       },
 
     },
