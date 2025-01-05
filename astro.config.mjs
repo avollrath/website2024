e6fdcf6e5048deaa0482fb82592bc4571fde02f4
Andre Vollrath <andre.vollrath@talentadore.com>
2025-01-05 21:37:53 +0200

    blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://vollrath.dev',
-  output: 'static',
-  outDir: './docs',
-  build: {
-    assets: 'astro'
-  },  
-  base: '/',
-});
+	integrations: [tailwind()],
+	site: 'https://vollrath.dev',
+	output: 'static',
+	outDir: './docs',
+	build: {
+		assets: 'astro'
+	},
+	base: '/'
+})
