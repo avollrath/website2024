73fd49de77fc812aef14d222068802fb2de97cab
Andre Vollrath <andre.vollrath@talentadore.com>
2025-01-30 07:49:45 +0200

    new build

---
+++ docs/astro/hoisted.Dmsnvv-C.js
@@ -0,0 +1,1 @@
+import"https://unpkg.com/@lottiefiles/lottie-player@2.0.8/dist/lottie-player.js";import"./hoisted.BAnJs-SD.js";
--- docs/astro/hoisted.DwFF_TtC.js
+++
@@ -1,1 +0,0 @@
-import"https://unpkg.com/@lottiefiles/lottie-player@2.0.8/dist/lottie-player.js";import"./hoisted.VQplzdks.js";
